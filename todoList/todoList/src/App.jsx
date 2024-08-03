bd56769e3a3439d6fe119f3c5657fafb73e293e6
Mukeshkanna <mukeshkanna.k2005@gmail.com>
2024-08-03 18:04:22 +0530

    updATE

--- todoList/todoList/src/App.jsx
+++ todoList/todoList/src/App.jsx
@@ -1,30 +1,50 @@
-import React, { useReducer } from 'react'
+import React, { useReducer } from 'react';
 
 const App = () => {
-  const intialValue=[
-    {id:1,name:test},
-    {id:2,name :reading}
-  ]
-  const [items,dipatcher]=useReducer(reeducer,intialValue)
+  const initialValue = [
+    { id: 1, name: "test" },
+    { id: 2, name: "reading" }
+  ];
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "add":
+        return [...state, { id: state.length + 1, name: action.payload }];
+      case "sub":
+        return state.filter((item) => item.id !== action.payload);
+      case "update":
+        return [...state];
+      default:
+        return state;
+    }
+  }
+
+  const [states, dispatcher] = useReducer(reducer, initialValue);
+
+  function handle(event) {
+    if (event.key === "Enter") {
+      dispatcher({ type: "add", payload: event.target.value });
+    }
+  }
+
   return (
     <>
-
-     <h1>
-      TODO List
-    </h1> 
-    <input/>
-    <button type="button">add</button>
-    <ol>
-   
-      items.map((item,index)=>{
-         <li>
-        exercise <button>delete</button>
-     </li>
-      }
-      )
-    </ol>
+      <h1>TODO List</h1>
+      <div>
+        <input onKeyDown={handle}></input>
+        <button onClick={() => { dispatcher({ type: "add", payload: prompt("Enter task") }) }}>Add</button>
+      </div>
+      <ul>
+        {states.map((task) => (
+          <div key={task.id}>
+            <li>{task.name}</li>
+            <button onClick={() => dispatcher({ type: "sub", payload: task.id })}>Delete</button>
+            <button onClick={() => dispatcher({ type: "update", payload: task.id  })}>update</button>
+          </div>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
