1f06d2303fee57ab5887fb77f514be634832ea4b
Mukeshkanna <mukeshkanna.k2005@gmail.com>
2024-08-15 06:52:35 +0530

    changw

--- foodDelivery/src/components/Navbar.jsx
+++ foodDelivery/src/components/Navbar.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import "./navbar.css"
-import logo from "/food/logo.jpg"
+import logo from "/food/logo.png"
 import {Link} from "react-router-dom"
 import homelogo from "/photo/home.svg"
 import cartlogo from "/photo/cart.svg"
+import { useContext } from 'react'
+import { CartContext } from '../App';
+
 const   Navbar = () => {
+  const { cart} = useContext(CartContext);
   return (
     <>
     <div className='container'>
         <div className='c_logo'>
-           <h2 >BiteOnTime</h2>
-           <img src={logo} className='logo'/>
+           <h2 >BiteMe</h2>
+           <img  src={logo} className='logo'/>
         </div>
         <nav>
         <div className='hom'>
           <img src={homelogo}></img>
         <Link to={"/" }>Home</Link>
         </div>
-        
+        <button> {cart.length}</button>
         <div className='hom'>
         <img src={cartlogo}></img>
 
